d704db0aed1efe61618470afb58d69848602774a
timaf <lo_sa_na@yahoo.com>
2018-04-05 20:51:39 +0200

    finshed

--- src/main/java/at/refugeescode/Mp17_TheThievesDen/ThievesEndPoint.java
+++ src/main/java/at/refugeescode/Mp17_TheThievesDen/ThievesEndPoint.java
@@ -12,4 +12,11 @@ public class ThievesEndPoint {
     String uncover(){
         return "The tresures:Gold,Money, Diamonds";
     }
+
 }
+
+/*Mp17 - The Thieves' Den
+        The Thieves Den web application has one single page displaying all the treasures that the thievery organization has been robbing for decades.
+         However, its access is not that easy, for it is secured tightly
+         and it won’t be shown unless the user “alibaba” logs in with the password “open sesame”.*/
+
