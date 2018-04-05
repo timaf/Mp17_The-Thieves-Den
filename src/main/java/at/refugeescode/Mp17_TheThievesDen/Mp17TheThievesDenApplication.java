5949ff8811ec0db4bbc667e3dadc9bb1ca1c6e18
timaf <lo_sa_na@yahoo.com>
2018-04-05 20:44:54 +0200

    start

---
+++ src/main/java/at/refugeescode/Mp17_TheThievesDen/Mp17TheThievesDenApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Mp17_TheThievesDen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp17TheThievesDenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp17TheThievesDenApplication.class, args);
+	}
+}
